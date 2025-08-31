0c79d7eb93745e6eb5ed4f56c42584abca27c3aa
Rahul Shah <rahulshah512@gmail.com>
2025-08-31 10:36:17 -0700

    Initial Commit

---
+++ DreamSculpt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DreamSculpt
+//
+//  Created by Rahul Shah on 8/31/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DreamSculpt/DreamSculptApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamSculptApp.swift
+//  DreamSculpt
+//
+//  Created by Rahul Shah on 8/31/25.
+//
+
+import SwiftUI
+
+@main
+struct DreamSculptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
